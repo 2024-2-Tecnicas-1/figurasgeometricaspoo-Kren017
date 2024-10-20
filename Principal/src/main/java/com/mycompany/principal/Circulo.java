59e344d46eba489b5fb4e4ab0863ad284d78bf8e
Juan <jersrodriguez0@gmail.com>
2024-10-20 13:42:59 -0500

    Cree las clases y su herencia

---
+++ Principal/src/main/java/com/mycompany/principal/Circulo.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.principal;
+
+public class Circulo extends FiguraGeometrica {
+
+    private double radio;
+
+    public Circulo(String nombre, String color, double radio) {
+        super(nombre, color);
+        this.radio = radio;
+    }
+
+    public double getRadio() {
+        return radio;
+    }
+
+    public void setRadio(double radio) {
+        this.radio = radio;
+    }
+
+    public double obtenerArea() {
+        return Math.PI * radio * radio;
+    }
+
+    public double obtenerPerimetro() {
+        return 2 * Math.PI * radio;
+    }
+}
---
+++ Principal/src/main/java/com/mycompany/principal/Triangulo.java
@@ -0,0 +1,41 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.principal;
+
+public class Triangulo extends FiguraGeometrica {
+
+    private double base;
+    private double altura;
+
+    public Triangulo(String nombre, String color, double base, double altura) {
+        super(nombre, color);
+        this.base = base;
+        this.altura = altura;
+    }
+
+    public double getBase() {
+        return base;
+    }
+
+    public void setBase(double base) {
+        this.base = base;
+    }
+
+    public double getAltura() {
+        return altura;
+    }
+
+    public void setAltura(double altura) {
+        this.altura = altura;
+    }
+
+    public double obtenerArea() {
+        return (base * altura) / 2;
+    }
+
+    public double obtenerPerimetro() {
+        return base + altura + Math.sqrt(base * base + altura * altura);
+    }
+}
